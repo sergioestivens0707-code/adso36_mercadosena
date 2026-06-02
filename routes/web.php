93dc4614bbf954b7b058c106123e87404d091136
sergio <sergioestivens0707@gmail.com>
2026-06-01 19:25:00 -0500

    definion de propiedades del modelo

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::resource('Categorias', CategoriaController::class);
